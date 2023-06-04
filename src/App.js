415d81ad1bc0afb702f500cdc7ba2275e6c522b5
mp-web3 <mp.web3@gmail.com>
2023-06-04 15:32:10 +0200

    refactored constructore state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import web3 from './web3';
 import lottery from './lottery.js'
 
 class App extends React.Component {
+  /*
   constructor(props) {
     super(props);
 
     this.state = { manager: ''};
   }
+  */
+ /* Refactor the constructor */
+
+ state = {
+  manager: ''
+ };
 
   async componentDidMount() { /*automatically called whenever the app component is placed on the screen*/
     const manager = await lottery.methods.manager().call();

b2a22de25ed849021453b62beb7d9812122e4f7a
mp-web3 <mp.web3@gmail.com>
2023-06-04 17:21:36 +0200

    refactored App.js using useState and useEffect instead of setState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import web3 from './web3';
 import lottery from './lottery.js'
 
-class App extends React.Component {
-  /*
-  constructor(props) {
-    super(props);
 
-    this.state = { manager: ''};
-  }
-  */
- /* Refactor the constructor */
+function App() {
 
+ const [ manager, setManager ] = useState('');
+ const [ players, setPlayers ] = useState([]);
+ const [ balance, setBalance ] = useState('');
+ const [ value, setValue ] = useState('');
+
+ useEffect(() => {
+  const fetchData = async () => {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    setManager(manager);
+    setPlayers(players);
+    setBalance(balance);
+  }; 
+  fetchData();
+ }, []);
+
+ /*
  state = {
   manager: '',
   players: [],
@@ -19,34 +30,35 @@ class App extends React.Component {
   value: ''
  };
 
-  async componentDidMount() { /*automatically called whenever the app component is placed on the screen*/
+  async componentDidMount() { 
     const manager = await lottery.methods.manager().call();
     const players = await lottery.methods.getPlayers().call();
     const balance = await web3.eth.getBalance(lottery.options.address);
     this.setState({ manager, players, balance });
   }
+  */
 
-  render() {
-    /*web3.eth.getAccounts().then(console.log);*/
+  /*web3.eth.getAccounts().then(console.log);*/
 
-    return (
-      <div>
-        <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}.
-          There are currently {this.state.players.length} people entered,
-          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
-        </p>
-        <hr />
-        <form>
-          <h4>Want to try your luck?</h4>
-          <div>
-            <label>Amount of ether to enter</label>
-            <input value={this.state.value} onChange={event => this.setState({ value: event.target.value })} />
-          </div>
-          <button>Enter</button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>This contract is managed by {manager}.
+        There are currently {players.length} people entered,
+        competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
+      <hr />
+      <form>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input value={value} onChange={e => setValue(e.target.value)} />
+        </div>
+        <button>Enter</button>
+      </form>
+    </div>
+  );
+  
 }
+
 export default App;
